fa5f699f8eca8116fe360949fe320e15333b1634
sohamdawn777 <sohamdawn12345@gmail.com>
2025-09-01 22:01:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+import L from "https://unpkg.com/leaflet@1.9.4/dist/leaflet-src.esm.js";
+
 const map= L.map("map", { center: [22.526911,88.377648], zoom: 19, maxZoom: 19, minZoom: 1 });
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {maxZoom: 19, minZoom: 1, tms: false }).addTo(map);
 
 let data= [{lat: 22.526911, lon: 88.377648, model: "https://raw.githubusercontent.com/sohamdawn777/Ar-map/main/model1.glb"}, {lat: 22.5999666, lon: 88.3729349, model: "https://raw.githubusercontent.com/sohamdawn777/Ar-map/main/model2.glb"}, {lat: 22.56492395, lon: 88.35405545738757, model: "https://raw.githubusercontent.com/sohamdawn777/Ar-map/main/model3.glb"}];
+
