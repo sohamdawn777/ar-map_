5a3b83b38d7c6140b324fb659af7c152480ef958
sohamdawn777 <sohamdawn12345@gmail.com>
2025-09-02 15:52:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -240,7 +240,8 @@ renderer.render(scene, camera);
 });
 }
 });
-*/for (let j of data) {
+
+/*for (let j of data) {
 
 const marker = L.marker([j.lat, j.lon], { 
     icon: L.icon({ iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png", iconSize: [32,32], iconAnchor: [16,32], popupAnchor: [0,-32] }),
