a8e46dcfe8511b7f0aa2e21da1f0a06d2701ba37
sohamdawn777 <sohamdawn12345@gmail.com>
2025-09-04 15:26:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -293,7 +293,7 @@ const map= L.map("map", { center: [22.526911,88.377648], zoom: 19, maxZoom: 19, 
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {maxZoom: 19, minZoom: 1, tms: false }).addTo(map);
 
-let data= [{lat: 22.526911, lon: 88.377648, model: "https://raw.githubusercontent.com/sohamdawn777/Ar-map/main/model1.glb", place: "Nabin Pally", link: https://q.me-qr.com/7ErN213N}, {lat: 22.5999666, lon: 88.3729349, model: "https://raw.githubusercontent.com/sohamdawn777/Ar-map/main/model2.glb", place: "Bidhan Sarani", link: https://qr1.me-qr.com/text/t49MjMpL}, {lat: 22.56492395, lon: 88.35405545738757, model: "https://raw.githubusercontent.com/sohamdawn777/Ar-map/main/model3.glb", place: "Lenin Sarani", link: https://qr1.me-qr.com/text/mhpVXPZ1}];
+let data= [{lat: 22.526911, lon: 88.377648, model: "https://raw.githubusercontent.com/sohamdawn777/Ar-map/main/model1.glb", place: "Nabin Pally", link: "https://q.me-qr.com/7ErN213N"}, {lat: 22.5999666, lon: 88.3729349, model: "https://raw.githubusercontent.com/sohamdawn777/Ar-map/main/model2.glb", place: "Bidhan Sarani", link: "https://qr1.me-qr.com/text/t49MjMpL"}, {lat: 22.56492395, lon: 88.35405545738757, model: "https://raw.githubusercontent.com/sohamdawn777/Ar-map/main/model3.glb", place: "Lenin Sarani", link: "https://qr1.me-qr.com/text/mhpVXPZ1"}];
 
 const scene= new THREE.Scene();
 
