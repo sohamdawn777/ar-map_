2078ffc86eecc03c8088ba6900fede0d858aed3e
sohamdawn777 <sohamdawn12345@gmail.com>
2025-09-04 20:25:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -20,12 +20,11 @@ function mapMarker (data) {
 for (let j of data) {
 const marker = L.marker([j.lat, j.lon], { 
     icon: L.icon({ iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png", iconSize: [32,32], iconAnchor: [16,32], popupAnchor: [0,-32] }),
-    title: "Graffiti Spot", draggable: false, riseOnHover: true, modelUrl: j.model }).addTo(map);
+    title: "Graffiti Spot", draggable: false, riseOnHover: true }).addTo(map);
 
 marker.bindPopup(`<h3>${j.place}</h3>
   <p>Want to know more about this place?<br>
-  <a href="${j.link}" target="_blank">Click here</a></p>
-`, { maxWidth: 200, minWidth: 50, autoPan: true, closeButton: true, keepInView: true });
+  <a href="${j.link}" target="_blank">Click here</a></p>`, { maxWidth: 200, minWidth: 50, autoPan: true, closeButton: true, keepInView: true });
 
 const bounds = L.latLngBounds(data.map(j => [j.lat, j.lon]));
 map.fitBounds(bounds);
@@ -35,7 +34,7 @@ const map= L.map("map", { center: [22.526911,88.377648], zoom: 19, maxZoom: 19, 
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {maxZoom: 19, minZoom: 1, tms: false }).addTo(map);
 
-let data= [{lat: 22.526911, lon: 88.377648, model: "https://raw.githubusercontent.com/sohamdawn777/Ar-map/main/model1.glb", place: "Nabin Pally", link: "https://q.me-qr.com/7ErN213N"}, {lat: 22.5999666, lon: 88.3729349, model: "https://raw.githubusercontent.com/sohamdawn777/Ar-map/main/model2.glb", place: "Bidhan Sarani", link: "https://qr1.me-qr.com/text/t49MjMpL"}, {lat: 22.56492395, lon: 88.35405545738757, model: "https://raw.githubusercontent.com/sohamdawn777/Ar-map/main/model3.glb", place: "Lenin Sarani", link: "https://qr1.me-qr.com/text/mhpVXPZ1"}];
+let data= [{lat: 22.526911, lon: 88.377648, place: "Nabin Pally", link: "https://q.me-qr.com/7ErN213N"}, {lat: 22.5999666, lon: 88.3729349, place: "Bidhan Sarani", link: "https://qr1.me-qr.com/text/t49MjMpL"}, {lat: 22.56492395, lon: 88.35405545738757, place: "Lenin Sarani", link: "https://qr1.me-qr.com/text/mhpVXPZ1"}];
 
 mapMarker(data);
 
